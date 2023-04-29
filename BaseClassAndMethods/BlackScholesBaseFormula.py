f0a29b78c1999514e73b7494f6fe85126a77c092
ryanwhling <ryanwhling@gmail.com>
2023-04-30 00:38:58 +0800

    FInished non-div BS model

--- BaseClassAndMethods/BlackScholesBaseFormula.py
+++ BaseClassAndMethods/BlackScholesBaseFormula.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from math import log, sqrt, exp
+from scipy.stats.norm import cdf
 
 class BlackScholeNonDividend(ABC):
     """The most basic Black-Scholes Formula that values a European Option on a stock that does not pay dividends before the option's expiry date
@@ -12,10 +14,40 @@ class BlackScholeNonDividend(ABC):
         call_put: 'c' for call, 'p' for put
     
     Returns:
-        Option Price for a non-dividend paying stock
+        Option Price for a non-dividend paying stock if .price is called
         
     """
 
-    def __init__(self, S: float, X: float, r: float, T: float, sigma: float, call_put: str) -> None:
+    def __init__(self, S: float, X: float, r: float, T: float, sigma: float, call_put: str) -> float:
         super().__init__()
-        assert 
+        assert isinstance(S, (float, int)) and (S>=0), "Spot Price - S shld be a non negative float"
+        assert isinstance(X, (float, int)) and (X>=0), "Strike/Exercise Price - X shld be a non negative float"
+        assert isinstance(r, (float, int)), "Risk Free Rate - r shld be a float" # rfr can be negative
+        assert isinstance(T, (float, int)) and (T>=0), "Time to maturity - T shld be a non negative float"
+        assert isinstance(sigma, (float, int)) and (sigma>=0), "Historical Vol - sigma shld be a non negative float"
+        assert isinstance(call_put, str) and (sigma in ['c', 'p']), r"call_put should be either 'c' or 'p'"
+
+        self.S = S
+        self.X = X
+        self.r = r
+        self.T = T
+        self.sigma = sigma
+        self.call_put = call_put
+    
+    @property
+    def d1(self):
+        return (log(self.S/self.X) + (self.r+(self.sigma**2)/2)*self.T)/(self.sigma*sqrt(self.T))
+
+    @property
+    def d2(self):
+        return self.d1 - (self.sigma * self.T)
+
+    @property
+    def price(self):
+        if self.call_put == 'c':
+            return self.S*cdf(self.d1) - self.X*(exp**(-self.r*self.T))*cdf(self.d2)
+        else:
+            return self.X*(exp**(-self.r*self.T))*cdf(-self.d2) - self.S*cdf(-self.d1)
+    
+
+
